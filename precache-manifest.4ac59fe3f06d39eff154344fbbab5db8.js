fb3321e7f420d61442f5a3b702aa96b1e71c77e6
vinay <vinaykr4321@gmail.com>
2020-05-26 10:47:52 +0530

    Initial Commit for TodoApp

---
+++ precache-manifest.4ac59fe3f06d39eff154344fbbab5db8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc8bb4ef59e85453668d6f925d3a7e3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d0097f90ae2e51e63c6",
+    "url": "/static/css/main.5fdafb79.chunk.css"
+  },
+  {
+    "revision": "d67a0dbc927c51d3e88b",
+    "url": "/static/js/2.b1863002.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b1863002.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d0097f90ae2e51e63c6",
+    "url": "/static/js/main.89113a5d.chunk.js"
+  },
+  {
+    "revision": "7c9d47fd73e17ab2e6dd",
+    "url": "/static/js/runtime-main.f291cd3a.js"
+  }
+]);
